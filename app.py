cc4c57b61c9e7c2166c993addf26bf6cc2d63086
kaif9711 <kaifmohammad2001@gmail.com>
2025-11-29 20:41:04 -0500

    Initial commit of assignment files

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    # Requirement: "Hello from lastname ECS Container"
+    return "Hello from Kaif ECS Container"
+
+
+@app.route('/health')
+def health():
+    return "Healthy", 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
